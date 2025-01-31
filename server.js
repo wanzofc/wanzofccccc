0d571131d80c90b98339e9d9ba1c52aebae3ec9b
wanzofc <berlianawan498@gmail.com>
2025-02-01 00:41:27 +0700

    Update server.js

--- server.js
+++ server.js
@@ -34,4 +34,4 @@ app.get("/api/tiktok-search", async (req, res) => {
     res.json({ results: data.results || [] });
 });
 
-app.listen(3000, () => console.log("Server berjalan di http://localhost:3000"));
+app.listen(8080, () => console.log("Server berjalan di http://localhost:8080"));
