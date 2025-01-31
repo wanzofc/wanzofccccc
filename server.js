13253aab99b48109b8527d30a574e8230813dfeb
wanzofc <berlianawan498@gmail.com>
2025-02-01 00:37:46 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const app = express();
+
+app.use(express.static(__dirname)); // Melayani index.html
+
+app.get("/api/tiktok", async (req, res) => {
+    const { url } = req.query;
+    const apiUrl = `https://api.awan-attack.my.id/tiktok?url=${url}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    res.json({
+        download_url: data.video_url || "",
+        thumbnail: data.thumbnail || ""
+    });
+});
+
+app.get("/api/igdl", async (req, res) => {
+    const { url } = req.query;
+    const apiUrl = `https://api.awan-attack.my.id/igdl?url=${url}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    res.json({
+        download_url: data.media || "",
+        thumbnail: data.thumbnail || ""
+    });
+});
+
+app.get("/api/tiktok-search", async (req, res) => {
+    const { query } = req.query;
+    const apiUrl = `https://api.awan-attack.my.id/s/tiktok?query=${query}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    res.json({ results: data.results || [] });
+});
+
+app.listen(3000, () => console.log("Server berjalan di http://localhost:3000"));
