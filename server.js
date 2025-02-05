4f8f289657669edc26c6ad54a9cfa354430300bd
wanzofc <berlianawan498@gmail.com>
2025-02-05 19:26:31 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.static(__dirname)); 
 const formatParagraph = (text) => text ? text.replace(/\.\s+/g, ".\n\n") : "Tidak ada jawaban.";
-app.get("/", (req, res) => {
+app.get("/kebijakan", (req, res) => {
     res.sendFile(path.join(__dirname, "kebijakan.html"));
 });
 app.get("/docs", (req, res) => {
