b3478d1190fd3938bac8b9d5b62b87c90707ac2b
wanzofc <berlianawan498@gmail.com>
2025-02-12 14:31:49 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(__dirname));
